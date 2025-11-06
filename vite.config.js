686dc3d95af1639097c59e1965004f5cafb49560
Mr_Adrs <adrshmishra018@gmail.com>
2025-11-06 20:02:29 +0530

    change origin url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:5000", // your backend
+        target: "https://t-p-app.onrender.com/", // your backend
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
