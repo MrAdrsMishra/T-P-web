1c071644457635b1973083edc342c8e30a09e81d
Mr_Adrs <adrshmishra018@gmail.com>
2025-11-07 12:48:44 +0530

    change proxy target url

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://t-p-app.onrender.com/", // your backend
+        target: "https://t-p-app.onrender.com", // your backend
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
